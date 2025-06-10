4aad3e59fca49d7be5f050a8bfe1c35907e366e3
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-06-10 12:24:11 +0200

    ex01

---
+++ sd00/ex01/main.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include "first_last.h"
+
+int main(int argc, char *argv[])
+{
+	int arr[] = {1, 2, 3, 4, 5, 3, 6};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int target = 1;
+	int first, last;
+
+	first_last(arr, size, target, &first, &last);
+
+	if (first != -1 && last != -1) {
+		printf("First occurrence of %d is at index %d\n", target, first);
+		printf("Last occurrence of %d is at index %d\n", target, last);
+	} else {
+		printf("%d not found in the array.\n", target);
+	}
+
+	return 0;
+}
