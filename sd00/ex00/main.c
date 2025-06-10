2f45b9cfd85a8498648426b70ccb1d9f1b3763e3
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-06-10 11:25:47 +0200

    tests

--- sd00/ex00/main.c
+++ sd00/ex00/main.c
@@ -4,13 +4,12 @@
 int main(int argc, char *argv[])
 {
 
-	int arr[] = {1, 2, 100}; // Example array
-	int size = sizeof(arr) / sizeof(arr[0]); // Calculate the size of the array
+	int arr[] = {010, 02, -100};
+	int size = sizeof(arr) / sizeof(arr[0]);
 
-	double result = average(arr, size); // Call the average function
+	double result = average(arr, size);
 
-	// Print the result to standard output
 	printf("Average: %.2f\n", result);
 
-	return 0; // Return success code
+	return (0);
 }
