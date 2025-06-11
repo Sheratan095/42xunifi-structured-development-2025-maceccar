a46e5b1d93a5579b3142e9e2d26fad78baa31a16
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-06-11 23:28:01 +0200

    ex08

---
+++ sd00/ex08/password_validator.h
@@ -0,0 +1,34 @@
+#ifndef PASSWORD_VALIDATOR_H
+#define PASSWORD_VALIDATOR_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef enum PwStatus
+{
+	VALID,
+	INVALID
+} PwStatus;
+
+typedef struct PasswordHistory
+{
+	char	*passwords[3];
+} PasswordHistory;
+
+// PwStatus	validate_password(const char *new_pw, const char *curr_pw);
+
+PwStatus	validate_password(const char *new_pw, PasswordHistory *history);
+
+int		ft_strlen(const char *str);
+
+bool	is_digit(const char c);
+
+bool	is_lowercase(const char c);
+
+bool	is_uppercase(const char c);
+
+bool	is_special_char(const char c);
+
+bool	ft_strcmp(const char *s1, const char *s2);
+
+#endif
